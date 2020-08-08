ae05d7bbaa17fc3874ff505d019c3a1be78fe588
raisuahmed <raisuahmed83@gmail.com>
2020-08-08 14:58:33 -0700

    true vs false

---
+++ true-false.js
@@ -0,0 +1,125 @@
+// const age = 4;
+
+// if (age > 0){
+//     console.log('condition is true');
+// }
+
+// else{
+//     console.log('condition is false');
+// }
+
+
+
+// const name = 'Rais U ahmed';
+
+// if (name.length > 0){
+//     console.log('condition is true');
+// }
+
+// else{
+//     console.log('condition is false');
+// }
+
+// const name = 'Rais U ahmed';
+
+// if (name.length){
+//     console.log('condition is true');
+// }
+
+// else{
+//     console.log('condition is false');
+// }
+
+
+//const name = '0';
+// if (name.length){
+//     console.log('condition is true');
+// }
+
+// else{
+//     console.log('condition is false');
+// }
+//ans: condition is true
+
+// let name = [];
+// console.log(name);
+// if (name){
+//         console.log('condition is true');
+//     }
+    
+//     else{
+//         console.log('condition is false');
+//     }
+  
+// ans:condition is true
+
+
+//empty string
+
+
+// const name = '';
+
+// if (name.length){
+//     console.log('condition is true');
+// }
+
+// else{
+//     console.log('condition is false');
+// }
+
+// ans: condition is false
+
+
+// const name = 0;
+// if (name.length){
+//     console.log('condition is true');
+// }
+
+// else{
+//     console.log('condition is false');
+// }
+
+// ans: condition is false
+
+// let name;
+// console.log(name);
+// if (name){
+//         console.log('condition is true');
+//     }
+    
+//     else{
+//         console.log('condition is false');
+//     }
+    
+
+// ans:condition is false
+
+
+//null
+
+// let name = null;
+// console.log(name);
+// if (name){
+//         console.log('condition is true');
+//     }
+    
+//     else{
+//         console.log('condition is false');
+//     }
+  
+// // ans:condition is false
+
+// NaN:
+
+// let name = NaN;
+// console.log(name);
+// if (name){
+//         console.log('condition is true');
+//     }
+    
+//     else{
+//         console.log('condition is false');
+//     }
+  
+// ans:condition is false
+
