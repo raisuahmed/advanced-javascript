fc5af2bf1e1da348729ea6fca8de70d6ce807d6b
raisuahmed <raisuahmed83@gmail.com>
2020-08-08 16:06:26 -0700

    double-vs-triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,73 @@
+//  const first = 2;
+//  const second = 2;
+//  if(first == second){
+//      console.log('condition is true');
+//  }
+
+//  else{
+//      console.log('condition is false');
+//  }
+ //ans:condition true
+
+//  const first = 2;
+//  const second = '2';
+//  if(first == second){
+//      console.log('condition is true');
+//  }
+
+//  else{
+//      console.log('condition is false');
+//  }
+ //ans:condition true
+
+
+ 
+//  const first = 1;
+//  const second = false;
+//  if(first == second){
+//      console.log('condition is true');
+//  }
+
+//  else{
+//      console.log('condition is false');
+//  }
+ //ans:condition false
+
+//  const first = 0;
+//  const second = false;
+//  if(first == second){
+//      console.log('condition is true');
+//  }
+
+//  else{
+//      console.log('condition is false');
+//  }
+ //ans:condition true
+
+
+ //triple equal:
+
+//  const first = 3;
+//  const second = true;
+//  if(first === second){
+//      console.log('condition is true');
+//  }
+
+//  else{
+//      console.log('condition is false');
+//  }
+ //ans:condition false
+
+
+ const first = 3;
+ const second = 3;
+ if(first === second){
+     console.log('condition is true');
+ }
+
+ else{
+     console.log('condition is false');
+ }
+
+
+ //ans:condition is true
