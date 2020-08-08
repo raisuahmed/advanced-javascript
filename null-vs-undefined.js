a27516a8b1f5f5db1323933726ce21a8257c5b3c
raisuahmed <raisuahmed83@gmail.com>
2020-08-08 15:32:18 -0700

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,45 @@
+let pakhi;
+console.log(pakhi);
+
+//ans: undefined
+
+// function add(num1, num2){
+// console.log(num1 + num2);
+
+// }
+
+// const result = add(12, 13);
+// console.log(result);
+//ans:undefined
+
+
+// function add(num1, num2){
+//     console.log(num2);
+    
+//     }
+    
+//     const result = add(12, 13);
+//     console.log(result);
+    //ans:undefined
+
+    
+    // function add(num1, num2){
+    //     console.log(num1, num2);
+     //    return
+    //     }
+        
+    //     const result = add( 13);
+    //     console.log(result);
+        //ans:undefined
+        
+    // const premik = {name:"smart", phone:445556};
+        
+//console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+//ans:undefined
+
+let ages = [12, 23, 13];
+console.log(ages[13]);
+//ans:undefined
