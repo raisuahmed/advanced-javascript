a37c91599e1145c75a48bef03569463995864f9e
raisuahmed <raisuahmed83@gmail.com>
2020-08-11 07:35:32 -0700

    array-split

---
+++ array-split.js
@@ -0,0 +1,17 @@
+// const nums = [11, 21, 31, 14, 15, 16, 72, 18];
+// const part = nums.slice(2, 5);
+// console.log(part);
+// console.log(nums);
+
+// const nums = [11, 21, 31, 14, 15, 16, 72, 18];
+// const remove = nums.splice(2, 5, 77);
+// console.log(remove);
+// console.log(nums);
+
+
+const nums = [11, 21, 31, 14, 15, 16, 2, 18];
+ const remove = nums.splice(2, 5, 77);
+
+
+const together = nums.join(" ")
+console.log(together);
